ac46757ab64536138424085147db0c5c85cfe597
fatmaaydin1214 <fatmaaydin1214@gmail.com>
2023-06-16 22:57:14 +0200

    first commit

--- src/test/java/test/C02_Get_ResponseBilgileriAssertion.java
+++ src/test/java/test/C02_Get_ResponseBilgileriAssertion.java
@@ -1,2 +1,36 @@
-package test;public class C02_Get_ResponseBilgileriAssertion {
+package test;
+
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class C02_Get_ResponseBilgileriAssertion {
+    /*
+        https://restful-booker.herokuapp.com/booking/10 url’ine bir GET request
+        gonderdigimizde donen Response’un,
+        status code’unun 200,
+        ve content type’inin application/json; charset=utf-8,
+        ve Server isimli Header’in degerinin Cowboy,
+        ve status Line’in HTTP/1.1 200 OK
+        oldugunu test edin.
+     */
+
+    @Test
+    public void get01(){
+        // URL hazirla
+        String url="https://restful-booker.herokuapp.com/booking/10";
+        //Expected data hazirla
+        //Response kaydet
+        Response response= given().when().get(url);
+        // response.prettyPrint();
+
+        //Assertion
+
+        response.then().assertThat()
+                .statusCode(200)
+                .contentType("application/json; charset=utf-8")
+                .header("Server","Cowboy")
+                .statusLine("HTTP/1.1 200 OK");
+    }
 }
--- src/test/java/test/C03_JsonObjesiOlusturma.java
+++ src/test/java/test/C03_JsonObjesiOlusturma.java
@@ -1,2 +1,56 @@
-package test;public class C03_JsonObjesiOlusturma {
+package test;
+
+import org.json.JSONObject;
+import org.junit.Test;
+
+public class C03_JsonObjesiOlusturma {
+     /*
+    Asagidaki JSON Objesini olusturup konsolda yazdirin.
+    {
+        "title":"Ahmet",
+        "body":"Merhaba",
+        "userId":1
+    }
+    */
+    @Test
+    public void jsonObje01(){
+        JSONObject ilkJsonObje =new JSONObject();
+
+        ilkJsonObje.put("title","Fatma");
+        ilkJsonObje.put("body","Merhaba");
+        ilkJsonObje.put("user id",1);
+
+        System.out.println("ilkJsonObje "+ ilkJsonObje);
+    }
+    @Test
+    public void jsonObje02(){
+        /*
+                {
+                 "firstname":"Jim",
+                 "additionalneeds":"Breakfast",
+                 "bookingdates":{
+                         "checkin":"2018-01-01",
+                         "checkout":"2019-01-01"
+                    },
+                  "totalprice":111,
+                  "depositpaid":true,
+                  "lastname":"Brown"
+                  }
+         */
+        JSONObject bookingDates = new JSONObject();
+        bookingDates.put("checkin","2018-01-01");
+        bookingDates.put("checkout","2019-01-01");
+
+        JSONObject outherJson = new JSONObject();
+
+        outherJson.put("firstname","Jim");
+        outherJson.put("additionalneeds","Breakfast");
+        outherJson.put("Booking dates", bookingDates);
+        outherJson.put("totalprice",111);
+        outherJson.put("depositpaid", true);
+        outherJson.put("lastname", "Brown");
+
+        System.out.println("Json obje02"+outherJson);
+
+    }
 }
