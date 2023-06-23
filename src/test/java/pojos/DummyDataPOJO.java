d3f471587eae37e813d56381707ba88d269d4ef1
fatmaaydin1214 <fatmaaydin1214@gmail.com>
2023-06-23 22:34:42 +0200

    23.06.2023

---
+++ src/test/java/pojos/DummyDataPOJO.java
@@ -0,0 +1,18 @@
+package pojos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class DummyDataPOJO {
+    private  int id;
+    private  String employee_name;
+    private int employee_salary;
+    private int employee_age;
+    private String profile_image;
+
+}
